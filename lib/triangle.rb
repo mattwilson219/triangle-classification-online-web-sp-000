ae1d3dba7cc3a24b5202afcc83d042cf4821c4b4
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-22 18:22:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,13 @@ def initialize(s1, s2, s3)
 end
 
 def kind
-      # first test for nonzero & nonnegative side lengths
-      # and test for "triangle inequality" principle
-    if  (s1 <= 0 || s2 <= 0 || s3 <= 0) ||
-        (s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2)
-        begin
-          raise TriangleError
-        #rescue TriangleError => error
-          puts error.message
-        end
+  if (s1 <= 0 && s2 <= 0) && s3 <= 0) &&
+  (s1 >= s2 + s3 && s2 >= s1 + s3 && s3 >= s1 + s2)
+ #((@s1 + @s2 <= @s3) && (@s2 + @s3 <= @s1) && (@s1 + @s3 <= @s2))
+ begin
+    raise TriangleError
+      puts error.message
+      end
 
 
 
