f8e10eb3ce95d26415accd7d43d66a421fb115e2
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-22 18:19:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,8 @@ def initialize(s1, s2, s3)
 end
 
 def kind
-  if ((@s1 <= 0) && (@s2 <= 0) && (@s3 <= 0)) &&
-  ((@s1 >= @s2 + @s3) && (@s2 >= @s1 + @s3) && (@s3 >= @s1 + @s2)
+  if ((s1 <= 0) && (s2 <= 0) && (s3 <= 0)) &&
+  ((s1 >= s2 + s3) && (s2 >= s1 + s3) && (s3 >= s1 + s2)
  #((@s1 + @s2 <= @s3) && (@s2 + @s3 <= @s1) && (@s1 + @s3 <= @s2))
  begin
     raise TriangleError
